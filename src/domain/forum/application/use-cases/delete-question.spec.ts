a099d849155e865319220759901db897d5701775
Jeferson Mesquita <je_2742@hotmail.com>
2023-05-09 21:35:12 -0300

    Caso de uso: Deletar pergunta

---
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -0,0 +1,56 @@
+import { makeQuestion } from 'test/factories/make-question';
+import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository';
+import { Slug } from '../../enterprise/entities/value-objects/slug';
+import { DeleteQuestionUseCase } from './delete-question';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: DeleteQuestionUseCase;
+// sut = system under test
+
+describe('Delete Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new DeleteQuestionUseCase(inMemoryQuestionsRepository);
+  });
+
+  it('should be able to delete question', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-id'),
+      },
+      new UniqueEntityID('question-id')
+    );
+
+    console.log(newQuestion);
+
+    await inMemoryQuestionsRepository.create(newQuestion);
+
+    await sut.execute({
+      questionId: 'question-id',
+      authorId: 'author-id',
+    });
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(0);
+  });
+
+  it('should not be able to delete a question from another user', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-id'),
+      },
+      new UniqueEntityID('question-id')
+    );
+
+    console.log(newQuestion);
+
+    await inMemoryQuestionsRepository.create(newQuestion);
+
+    expect(() => {
+      return sut.execute({
+        questionId: 'question-id',
+        authorId: 'author-id-2',
+      });
+    }).rejects.toBeInstanceOf(Error);
+  });
+});
