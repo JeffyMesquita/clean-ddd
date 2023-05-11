f10649a06e771fe0b49ce8f2279bcde281d381cf
Jeferson Mesquita <je_2742@hotmail.com>
2023-05-10 21:20:29 -0300

    Caso de uso: Editar resposta

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,38 +1,46 @@
-import { QuestionsRepository } from '@/domain/forum/application/repositories/question-repository'
-import { Question } from '@/domain/forum/enterprise/entities/question'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/question-repository';
+import { Question } from '@/domain/forum/enterprise/entities/question';
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
-  public items: Question[] = []
+  public items: Question[] = [];
 
   async findById(id: string): Promise<Question | null> {
     const question = this.items.find(
-      (question) => question.id.toString() === id,
-    )
+      (question) => question.id.toString() === id
+    );
 
     if (!question) {
-      return null
+      return null;
     }
 
-    return question
+    return question;
   }
 
   async findBySlug(slug: string): Promise<Question | null> {
-    const question = this.items.find((question) => question.slug.value === slug)
+    const question = this.items.find(
+      (question) => question.slug.value === slug
+    );
 
     if (!question) {
-      return null
+      return null;
     }
 
-    return question
+    return question;
+  }
+
+  async save(question: Question) {
+    const itemIndex = this.items.findIndex((item) => item.id === question.id);
+
+    this.items[itemIndex] = question;
   }
 
   async create(question: Question) {
-    this.items.push(question)
+    this.items.push(question);
   }
 
   async delete(question: Question) {
-    const itemIndex = this.items.findIndex((item) => item.id === question.id)
+    const itemIndex = this.items.findIndex((item) => item.id === question.id);
 
-    this.items.splice(itemIndex, 1)
+    this.items.splice(itemIndex, 1);
   }
 }
