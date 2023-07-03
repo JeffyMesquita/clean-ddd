bd7855d7398f55f09845911686a7c24d0f567051
Jeferson Mesquita <je_2742@hotmail.com>
2023-07-03 11:06:12 -0300

    Caso de uso: Listar comentários da pergunta

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination.params';
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository';
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment';
 
@@ -16,6 +17,15 @@ export class InMemoryQuestionCommentsRepository
     }
 
     return questionComment;
+  }
+
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const questionComments = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20);
+
+    return questionComments;
   }
 
   async create(questionComments: QuestionComment) {
