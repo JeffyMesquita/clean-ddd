0285f63ae079c2a7225aba341fe72d0d1c0a0b39
Jeferson Mesquita <je_2742@hotmail.com>
2023-07-03 11:14:48 -0300

    Caso de uso: Listar comentários da resposta

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination.params';
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository';
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment';
 
@@ -12,6 +13,15 @@ export class InMemoryAnswerCommentsRepository
     if (!answerComment) {
       return null;
     }
+
+    return answerComment;
+  }
+
+  async findManyByAnswerId(questionId: string, { page }: PaginationParams) {
+    const answerComment = this.items
+      .filter((item) => item.answerId.toString() === questionId)
+      .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+      .slice((page - 1) * 20, page * 20);
 
     return answerComment;
   }
