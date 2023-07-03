74eea3e07ceae9886617f166e43799eb41f5f922
Jeferson Mesquita <je_2742@hotmail.com>
2023-07-03 10:47:15 -0300

    Caso de uso: Deletar comentário da resposta

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -6,7 +6,25 @@ export class InMemoryAnswerCommentsRepository
 {
   public items: AnswerComment[] = [];
 
+  async findById(id: string) {
+    const answerComment = this.items.find((item) => item.id.toString() === id);
+
+    if (!answerComment) {
+      return null;
+    }
+
+    return answerComment;
+  }
+
   async create(answerComments: AnswerComment) {
     this.items.push(answerComments);
+  }
+
+  async delete(answerComments: AnswerComment) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.id === answerComments.id
+    );
+
+    this.items.splice(itemIndex, 1);
   }
 }
