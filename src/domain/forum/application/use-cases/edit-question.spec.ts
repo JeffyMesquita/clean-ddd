b0648d765fc799e8d5a79691cacc787aff601d94
Jeferson Mesquita <je_2742@hotmail.com>
2023-07-04 11:06:35 -0300

    feat:: Teste de edição da pergunta

--- src/domain/forum/application/use-cases/edit-question.spec.ts
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -3,15 +3,23 @@ import { makeQuestion } from 'test/factories/make-question';
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository';
 import { EditQuestionUseCase } from './edit-question';
 import { NotAllowedError } from './errors/not-allowed-error';
+import { InMemoryQuestionAttachmentsRepository } from 'test/repositories/in-memory-question-attachments-repository';
+import { makeQuestionAttachments } from 'test/factories/make-question-attachment';
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
 let sut: EditQuestionUseCase;
 // sut = system under test
 
 describe('Delete Question', () => {
   beforeEach(() => {
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
-    sut = new EditQuestionUseCase(inMemoryQuestionsRepository);
+    inMemoryQuestionAttachmentsRepository =
+      new InMemoryQuestionAttachmentsRepository();
+    sut = new EditQuestionUseCase(
+      inMemoryQuestionsRepository,
+      inMemoryQuestionAttachmentsRepository
+    );
   });
 
   it('should be able to edit a question', async () => {
@@ -24,17 +32,42 @@ describe('Delete Question', () => {
 
     await inMemoryQuestionsRepository.create(newQuestion);
 
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachments({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityID('1'),
+      }),
+      makeQuestionAttachments({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityID('2'),
+      })
+    );
+
     await sut.execute({
       questionId: newQuestion.id.toValue(),
       authorId: 'author-id',
       title: 'new title',
       content: 'new content',
+      attachmentsIds: ['1', '3'],
     });
 
     expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
       title: 'new title',
       content: 'new content',
     });
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.currentItems
+    ).toHaveLength(2);
+    expect(
+      inMemoryQuestionsRepository.items[0].attachments.currentItems
+    ).toEqual([
+      expect.objectContaining({
+        attachmentId: new UniqueEntityID('1'),
+      }),
+      expect.objectContaining({
+        attachmentId: new UniqueEntityID('3'),
+      }),
+    ]);
   });
 
   it('should not be able to delete a question from another user', async () => {
@@ -52,6 +85,7 @@ describe('Delete Question', () => {
       authorId: 'author-id-2',
       title: 'new title',
       content: 'new content',
+      attachmentsIds: [],
     });
 
     expect(result.isLeft()).toBeTruthy();
